11ee96857bb01ec31764f9a1a8152ed0bf002690
Emir Soyturk <emirsytrk@gmail.com>
2023-10-22 10:30:51 +0300

    fe prove attempts

--- web/src/api/circuit/index.js
+++ web/src/api/circuit/index.js
@@ -1,65 +1,7 @@
-import {decompressSync} from "fflate";
 import {ethers} from "ethers";
-import __wbg_init, {executeCircuit, compressWitness} from "@noir-lang/acvm_js";
-import {newBarretenbergApiAsync, RawBuffer, Crs, newBarretenbergApiSync} from "@aztec/bb.js/dest/browser/index.js";
 import circuit from './sentiment.json'
-
-function decompression() {
-    const acirBuffer = Buffer.from(circuit.bytecode, 'base64');
-    const acirBufferUncompressed = decompressSync(acirBuffer);
-
-    return {
-        acirBufferUncompressed,
-        acirBuffer
-    }
-}
-
-async function initializeBB(acirBufferUncompressed) {
-    const api = await newBarretenbergApiSync();
-
-    const [exact, circuitSize, subgroup] = await api.acirGetCircuitSizes(acirBufferUncompressed);
-    const subgroupSize = Math.pow(2, Math.ceil(Math.log2(circuitSize)));
-    const crs = await Crs.new(subgroupSize + 1);
-    await api.commonInitSlabAllocator(subgroupSize);
-    await api.srsInitSrs(new RawBuffer(crs.getG1Data()), crs.numPoints, new RawBuffer(crs.getG2Data()));
-
-    const acirComposer = await api.acirNewAcirComposer(subgroupSize);
-
-    return {
-        acirComposer,
-        api
-    }
-}
-
-async function generateWitness(input, hash, positive, acirBuffer) {
-    const initialWitness = new Map();
-
-    const public_inputs = [
-        ...input.map(x => ethers.utils.hexZeroPad(ethers.utils.hexlify(x), 32)),
-        ethers.utils.hexZeroPad(ethers.utils.hexlify(positive), 32),
-        hash,
-    ]
-
-    for (let i = 0; i < public_inputs.length; i++) {
-        initialWitness.set(i + 1, public_inputs[i]);
-    }
-
-    const witnessMap = await executeCircuit(acirBuffer, initialWitness, () => {
-        throw Error('unexpected oracle');
-    });
-
-    return compressWitness(witnessMap);
-}
-
-async function generateProof({witness, api, acirComposer, acirBufferUncompressed}) {
-    const proof = await api.acirCreateProof(
-        acirComposer,
-        acirBufferUncompressed,
-        decompressSync(witness),
-        false,
-    );
-    return proof;
-}
+import {BarretenbergBackend} from '@noir-lang/backend_barretenberg';
+import {Noir} from '@noir-lang/noir_js';
 
 export async function feProve({
                                   input,
@@ -67,24 +9,21 @@ export async function feProve({
                                   positive
                               }) {
     try {
-        const o = await __wbg_init()
-        const acirBuffers = decompression()
-        const bb = await initializeBB(acirBuffers.acirBufferUncompressed)
-        const witness = await generateWitness(input, hash, positive, acirBuffers.acirBuffer)
+        const backend = new BarretenbergBackend(circuit);
+        const noir = new Noir(circuit, backend);
+        await noir.init();
+        const publicInputs = {
+            input: input.map(x => ethers.utils.hexZeroPad(ethers.utils.hexlify(x), 32)),
+            hash: hash,
+            intent: ethers.utils.hexZeroPad(ethers.utils.hexlify(positive ? 1 : 0), 32)
+        }
 
-        const proof = await generateProof({
-            witness: witness,
-            api: bb.api,
-            acirComposer: bb.acirComposer,
-            acirBufferUncompressed: acirBuffers.acirBufferUncompressed
-        })
-
-        let hex = Buffer.from(proof).toString('hex');
-
-        await bb.api.destroy()
+        console.log(publicInputs)
+        console.log(positive)
+        const proof = await noir.generateFinalProof(publicInputs);
 
         return {
-            proof: hex
+            proof: proof
         }
     } catch (e) {
         console.log(e)
--- web/vite.config.js
+++ web/vite.config.js
@@ -1,10 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import {nodePolyfills} from "vite-plugin-node-polyfills";
+import copy from 'rollup-plugin-copy';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-      react()
-  ],
-})
+const wasmContentTypePlugin = {
+    name: 'wasm-content-type-plugin',
+    configureServer(server) {
+        server.middlewares.use(async (req, res, next) => {
+            if (req.url.endsWith('.wasm')) {
+                res.setHeader('Content-Type', 'application/wasm');
+                const newPath = req.url.replace('deps', 'dist');
+                const targetPath = path.join(__dirname, newPath);
+                const wasmContent = fs.readFileSync(targetPath);
+                return res.end(wasmContent);
+            }
+            next();
+        });
+    },
+};
+
+export default defineConfig(({ command }) => {
+    if (command === 'serve') {
+        return {
+            plugins: [
+                copy({
+                    targets: [{ src: 'node_modules/**/*.wasm', dest: 'node_modules/.vite/dist' }],
+                    copySync: true,
+                    hook: 'buildStart',
+                }),
+                command === 'serve' ? wasmContentTypePlugin : [],
+                react()
+            ],
+        };
+    }
+
+    return {
+        plugins: [
+            react()
+        ]
+    };
+});
